a0741f712c483440f63582477a97ae3069e833d7
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-01-10 08:37:38 -0300

    feat: :art: add theme

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,11 +2,12 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600}
   justify-content: center;
   align-items: center;
 `;
 
 export const Title = styled.Text`
-  color: #000;
+  color: ${({ theme }) => theme.COLORS.WHITE}
   font-size: 26px;
 `;
