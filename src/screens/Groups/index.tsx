cd47e6bea3f2a7d8d9715e0ae1f566ec649c5bd1
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-01-14 15:15:08 -0300

    feat: :sparkles: Add groupcard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
 import { Highlight } from '@components/Highlight';
 
 import { Container } from './styles';
@@ -8,6 +9,8 @@ export function Groups() {
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <GroupCard title="Groups" />
     </Container>
   );
 }
