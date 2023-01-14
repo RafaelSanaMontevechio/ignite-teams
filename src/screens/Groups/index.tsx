b98b2248f7110b4ed3d2f8a8df6d63aa84c69093
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-01-14 18:41:06 -0300

    feat: :sparkles: Add emptyList component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
-import { GroupCard } from '@components/GroupCard';
+import { EmptyList } from '@components/EmptyList';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Groups" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={<EmptyList message="Nenhuma turma criada" />}
+      />
     </Container>
   );
 }
