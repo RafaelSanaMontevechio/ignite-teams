4382dd83cf9f568af6fe092c22b1b452d1e14fb4
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-02-01 08:50:49 -0300

    feat: :sparkles: add players list

--- src/screens/players/index.tsx
+++ src/screens/players/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+
+import { FlatList } from 'react-native';
+
 import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 
-import { Container, Form } from './styles';
-import { Filter } from '@components/Filter';
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -18,7 +25,22 @@ export function Players() {
         <ButtonIcon icon="add" />
       </Form>
 
-      <Filter title="Time A" />
+      <HeaderList>
+        <FlatList
+          horizontal
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+        />
+
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
