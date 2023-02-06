a7453f0270fd3eeaa1261cd6248c5660d2ed4478
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-02-06 09:20:06 -0300

    feat: :ambulance: add creating and list group

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+
 import { FlatList } from 'react-native';
+
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import { groupGetAll } from '@storage/group/groupGetAll';
 
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
@@ -8,16 +13,31 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 
 import { Container } from './styles';
-import { useNavigation } from '@react-navigation/native';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
   const navigation = useNavigation();
 
+  const fetchGroups = async () => {
+    try {
+      const data = await groupGetAll();
+
+      setGroups(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const handleNewGroup = () => {
     navigation.navigate('newGroup');
   };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, []),
+  );
 
   return (
     <Container>
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
+import { groupCreate } from '@storage/group/groupCreate';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
@@ -14,8 +16,13 @@ export function NewGroup() {
 
   const navigation = useNavigation();
 
-  const handleNewGroup = () => {
-    navigation.navigate('players', { group: newGroup });
+  const handleNewGroup = async () => {
+    try {
+      await groupCreate(newGroup);
+      navigation.navigate('players', { group: newGroup });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
